13b1484b827a2fb707b48d51bccadffc7ca0ba05
Gal JacobSon <Jacobsongal@gmail.com>
2020-06-09 18:00:53 +0300

    db

---
+++ RealEstate/Models/OfficeDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace RealEstateOffice.Models
+{
+    public class MyDB : DbContext
+    {
+        public DbSet<Customer> customers { get; set; }
+
+        public DbSet<Deal> deals { get; set; }
+
+        public DbSet<Property> properties { get; set; }
+
+        public DbSet<Broker> brokers { get; set; }
+
+    }
+}
